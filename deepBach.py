ae6fa78b8b29385d0211b01cd4fc5736ec476122
Konzert <HelloGitWorld@gmail.com>
2017-11-22 17:01:30 -0600

    Changed Hyper-Parameters
    Removed epsilon and decay arguments

--- deepBach.py
+++ deepBach.py
@@ -678,7 +678,8 @@ def train_models(model_name, steps_per_epoch, num_epochs, validation_steps, time
         model_path_name = 'models/' + model_name + '_' + str(voice_index)
 
         model = load_model(model_path_name)
-        adam = optimizers.Adam(lr=0.0001, epsilon=1e-05, decay = .0001)
+        #adam = optimizers.Adam(lr=0.0001, epsilon=1e-05, decay = .0001) Used up to 11/22/2017 on Azure
+        adam = optimizers.Adam(lr=0.0001) # removing non lr parameters
         model.compile(optimizer=adam, loss={'pitch_prediction': 'categorical_crossentropy'
                                               },
                       metrics=['accuracy'])
